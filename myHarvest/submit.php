9d0d29744b120ffe79369a350c75261217dc9ec6
Paige Kohn <pkohn@student.touro.edu>
2018-03-28 20:49:40 -0400

    updated order form, and cart to work

--- myHarvest/submit.php
+++ myHarvest/submit.php
@@ -1,19 +1,23 @@
-<!DOCTYPE html>
-<html>          
-<?php 
+<?php
+session_start();
 include "header.php";
 include "config.php";
 
 ?>
-<title>Order Complete!</title>  
-       
+<?php
+if (isset($_POST['submit'], $_POST['cust_name'])) {
+    $sql = "SELECT * FROM products";
+}
+?>
+<title>Order Complete!</title>
+
 <body>
-<canvas style=height:30px;></canvas>
+	<canvas style="height: 30px;"></canvas>
 
-<div style=text-align:left;margin-left:520px;padding:24px;></div>
+	<div style="text-align: left; margin-left: 520px; padding: 24px;"></div>
 
-<h2>Order Summary</h2>
-<?php 
+	<h2>Order Summary</h2>
+<?php
 $CUST_NAME = $_POST['cust_name'];
 $DATE = $_POST['date'];
 $TEL = $_POST['tel'];
@@ -24,55 +28,98 @@ $ZIP = $_POST['zip'];
 $DELIVERY = $_POST['delivery'];
 
 
-$TOTAL = "44.11";
 
+mysqli_query($db, "INSERT INTO ORDERS (CUST_TEL, ORDER_DATE, ORDER_TYPE_ID, ORDER_TOTAL,  CUSTOMERS_CUST_ID) 
+VALUES ('$TEL', '$DATE', '$DELIVERY', '$total',  '$TEL')");
 
-
-mysqli_query($db,"INSERT INTO ORDERS (CUST_TEL, ORDER_DATE, ORDER_TYPE_ID, ORDER_TOTAL,  CUSTOMERS_CUST_ID) 
-VALUES ('$TEL', '$DATE', '$DELIVERY', '$TOTAL',  '$TEL')");
-
-
-
-
-if(mysqli_affected_rows($db)>0){
-     mysqli_affected_rows($db);
-     
-}
-else{
-   
+$orderid = mysqli_insert_id($db);
+echo $orderid;
+if (mysqli_affected_rows($db) > 0) {
+    mysqli_affected_rows($db);
+} else {
+    
     echo mysqli_error($db);
 }
 ?>
 
 <b>DATE:&nbsp;&nbsp;</b><?php echo $DATE ?><br>
-<b>NAME:&nbsp;&nbsp;</b><?php echo $CUST_NAME ?><br><br>
-<b>&nbsp;&nbsp;TEL:&nbsp;&nbsp;</b><?php echo $TEL ?><br>
-<b>ADDRESS:&nbsp;&nbsp;</b><?php echo $ADDRESS?><br>
-<b>&nbsp;&nbsp;</b><?php echo $CITY ?><br>
-<b>&nbsp;&nbsp;</b><?php echo $STATE ?><br>
-<b>&nbsp;&nbsp;</b><?php echo $ZIP ?><br>
-<br><?php echo $qty ?><br>
-<br><?php echo $item_no ?><br>
-<br><?php echo $item_desc ?><br>	
+	<b>NAME:&nbsp;&nbsp;</b><?php echo $CUST_NAME ?><br>
+	<br>
+	<b>&nbsp;&nbsp;TEL:&nbsp;&nbsp;</b><?php echo $TEL ?><br>
+	<b>ADDRESS:&nbsp;&nbsp;</b><?php echo $ADDRESS?><br>
+	<b>&nbsp;&nbsp;</b><?php echo $CITY ?><br>
 
-<?php echo $TOTAL?>
+	<b>&nbsp;&nbsp;</b><?php echo $STATE ?><br>
+	<b>&nbsp;&nbsp;</b><?php echo $ZIP ?><br>
+	<b>&nbsp;&nbsp;<?php echo 'QTY'."\t\t" ?></b>
+	<b>&nbsp;&nbsp;<?php echo 'ITEM DETAIL'."\t\t" ?></b>
+	<b>&nbsp;&nbsp;<?php echo 'PRICE'."\t\t" ?></b>
+	<b>&nbsp;&nbsp;<?php echo 'SUBTOTAL' ."\t\t"?></b><br>
+
+<?php
+
+foreach ($_POST['qty'] as $itemNo=>$itemQty) {
+    if (! ($itemQty == 0 || $itemQty== "")) {
+        
+        
+        $sql = "SELECT * FROM products WHERE ITEM_NO = $itemNo";
+        $result = mysqli_query($db, $sql);
+        
+        if ($row = mysqli_fetch_array($result)) {
+            echo $itemQty."\t\t\t"."&nbsp;&nbsp;";
+            echo $itemWeight = $row["ITEM_WEIGHT"]."\t\t\t"."&nbsp;&nbsp;";    
+            echo $itemDesc = $row["ITEM_DESC"]."\t\t\t"."&nbsp;&nbsp;";
+            echo $price = $row["ITEM_PRICE"]."\t\t\t"."&nbsp;&nbsp;";
+                    
+           
+            $subtotal = $price*$itemQty;
+            
+            $total+= $subtotal;
+            echo $subtotal."<br>";
+            $sql= "INSERT INTO ORDER_LINE_ITEM (ITEM_DESC, ITEM_NO, ITEM_WEIGHT_TYPE, ORDER_ID,  PRICE, QUANTITY)
+            VALUES ('$itemDesc', $itemNo, '$itemWeight', $orderid,  $price, $itemQty)";
+            $result = mysqli_query($db, $sql);
+            
+          
+           $_SESSION['cart'][$itemNo]['qty']="";
+        }
+    }
+}
+
+echo "total:".$total;
+//update order total with actual total
+$sql= "UPDATE ORDERS SET ORDER_TOTAL = $total WHERE ORDER_ID = $orderid";
+     
+$result = mysqli_query($db, $sql);
+
+
+
+
+?>
+
+
+
 
 
 
 
 <br>
-<blockquote><b> Thank You For Your Order. A copy of this order has been sent to your email. </b>
-</blockquote>
+	<blockquote>
+		<b> Thank You For Your Order. A copy of this order has been sent to
+			your email. </b>
+	</blockquote>
 
 
 
-<p><canvas style=height:200px;></canvas>
+	<p>
+		<canvas style="height: 200px;"></canvas>
+
 </body>
-<?php 
+<?php
 
 ?>
 <?php include "footer.php";?>
-<?php 
+<?php
 
 function check_input($data)
 {
